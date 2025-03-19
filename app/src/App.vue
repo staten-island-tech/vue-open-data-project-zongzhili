7d97543eda02351cc510df29e6be48a16eb33de6
zongzhili <zongzhil@nycstudents.net>
2025-03-19 14:01:05 -0400

    aa

--- app/src/App.vue
+++ app/src/App.vue
@@ -14,4 +14,29 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <div>
       <Card v-for="item in death" :key="item.leading_cause + item.year" :data="item" />
     </div>
-    <Bar v-if="loaded" :data="death" />
+    <Bar v-if="loaded" :data="chartData" />
   </div>
 </template>
 
@@ -13,12 +13,52 @@ import Card from '@/components/DataCard.vue'
 import { ref, onMounted } from 'vue'
 import { Bar } from 'vue-chartjs'
 
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 const death = ref([])
+const loaded = ref(false)
+
+const chartData = ref({
+  labels: [],
+  datasets: [
+    {
+      label: 'Number of Deaths',
+      data: [],
+      backgroundColor: 'rgba(66, 165, 245, 0.6)',
+    },
+  ],
+})
+
 async function getData() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json') //?$limit=100
+  let res = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
   let data = await res.json()
-  console.log(data)
-  death.value = data
+  // console.log(data)
+  // death.value = data
+  const causes = {}
+
+  data.forEach((item) => {
+    const cause = item.leading_cause
+    if (cause) {
+      if (!causes[cause]) {
+        causes[cause] = 0
+      }
+      causes[cause] += parseInt(item.deaths) || 0
+    }
+  })
+
+  chartData.value.labels = Object.keys(causes)
+  chartData.value.datasets[0].data = Object.values(causes)
+
+  loaded.value = true
 }
 onMounted(() => {
   getData()
